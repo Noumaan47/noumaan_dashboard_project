123ad5e76094332e99a9cd9247b7fef53d305dbf
Noumaan47 <153702889+Noumaan47@users.noreply.github.com>
2023-12-30 23:20:29 +0530

    Add files via upload

---
+++ game.java
@@ -0,0 +1,89 @@
+
+
+import javax.swing.*;
+import java.util.Random;
+
+public class game {
+
+    private final int actualValue;
+    private int min;
+    private int max;
+    private int attempts = 3;
+    int userNumber;
+
+    public game() {
+        setMin(0);
+        setMax(100);
+        actualValue = generateRandom();
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setUserNumber(int userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public int getActualValue() {
+        return actualValue;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int generateRandom() {
+        return new Random().nextInt(min, max);
+    }
+
+    public void checkCorrect(UI obj) {
+        if (getActualValue() == userNumber) {
+            JOptionPane.showMessageDialog(obj, "YOU WIN!");
+            refreshGame(obj);
+        }
+        else {
+            JOptionPane.showMessageDialog(obj, "YOU LOOSE!");
+            attempts--;
+        }
+    }
+
+    public void gameOver(UI obj) {
+        obj.inputField.setEditable(false);
+        obj.enterButton.setEnabled(false);
+        obj.correctLabel.setText("Actual value: " + actualValue);
+        obj.correctLabel.setVisible(true);
+        obj.refreshButton.setEnabled(true);
+    }
+
+    public void refreshGame(UI obj) {
+        attempts = 3;
+        obj.game = new game();
+        obj.inputField.setEditable(true);
+        obj.enterButton.setEnabled(true);
+        obj.correctLabel.setVisible(false);
+        obj.refreshButton.setEnabled(false);
+        obj.attemptsLabel.setText("Attempts left: " + getAttempts());
+    }
+
+    public void range() {
+        int difference = actualValue - userNumber;
+        if (difference >= 30)
+            System.out.println("Too low");
+        else if (difference <= -30)
+            System.out.println("Too High");
+    }
+
+}
